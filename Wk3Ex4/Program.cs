d2a0dc8f60cd14c5917a11b0ae56e5740d1e2938
Jasmine Ramirez-Miller <jassymill@yahoo.com>
2025-02-03 01:09:59 -0600

    updating

--- Wk3Ex4/Program.cs
+++ Wk3Ex4/Program.cs
@@ -10,7 +10,68 @@ namespace Wk3Ex4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 4");
+            //Inital balance 
+            double balance = 1000;
+
+            //loop for the options 
+            while (true)
+            {
+                //display ATM menu
+                Console.WriteLine("\nATM Menu:");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Exit");
+
+                Console.Write("Choose an Option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option) 
+                {
+                    case 1: //check balance
+                        Console.WriteLine("Your balance is:" + balance);
+                        break;
+                        case 2: //Deposit
+                        Console.Write("Enter deposit amount: ");
+                        double deposit = Convert.ToDouble(Console.ReadLine());
+                        if (deposit > 0)
+                        {
+                            balance += deposit;
+                            Console.WriteLine("Deposit successful");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("Invalid deposit amount.");
+                        }
+                        break;
+                        case 3: //Withdraw
+                        Console.Write("Enter withdrawal amount: ");
+                        double withdraw = Convert.ToDouble(Console.ReadLine());
+                        if (withdraw > 0 && withdraw <= balance) 
+                        {
+                            balance -= withdraw;
+                            Console.WriteLine("Withdrawal sucessful.");
+                        }
+                        else if (withdraw > balance)
+                        {
+                            Console.WriteLine("Insufficient balance.");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("Invalid withdraw amount.");
+                        }
+                        break;
+                        case 4: //Exit 
+                            Console.WriteLine("Thank you for using the ATM. Goodbye!");
+                            return;
+                    default: //Invalid option
+                        Console.WriteLine("Invalid option. Please choose between 1 and 4.");
+                        break
+
+
+                }
+
+            }
         }
     }
 }
